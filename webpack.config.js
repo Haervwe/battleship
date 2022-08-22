cfa7dd6a4ccef48fa3d9e2b81945cb769de6b7b0
Haervwe <juan-jose-007@hotmail.com>
2022-08-22 18:50:56 -0300

    add modules, and basic file structure

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
